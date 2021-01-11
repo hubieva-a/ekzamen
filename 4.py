429c5154b5850de24d831a053c0b4b2c1c2fd4d1
hubieva-a <72037369+hubieva-a@users.noreply.github.com>
2021-01-11 11:51:40 +0300

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,10 @@
+n=int(input("How much did you get "))
+if n < 50:
+    z=n*0.3:
+elif n > 50 and n < 75:
+    z = n * 0.5
+elif n > 75 and n < 90:
+    z = n * 0.65
+elif n > 90:
+    z = n * 0.65
+print(z)
